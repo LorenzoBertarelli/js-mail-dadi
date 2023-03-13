bd1df8ef5f729c324cfa5960a51ae4f49f63bccc
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-03-13 17:32:52 +0100

    genearatore numeri random

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// MAIL
+
 // CHIEDERE L'MAIL ALL'UTENTE
 const mailUtente = prompt("Inserisci la tua mail")
 
@@ -9,15 +11,26 @@ let result = "Benvenuto!";
 
 for (let i = 0; i < worldMail.length; i++) {
     const Mail = worldMail[i];
-    console.log (Mail)
+    console.log (Mail);
     if (Mail === mailUtente) {
         result = true;  
     }
 }
 
 if (result === true) {
-    console.log("Benvenuto nella pagina")
+    console.log("Benvenuto nella pagina");
 } else {
-    console.log("Mail sbagliata, riprova!")
-}
+    console.log("Mail sbagliata, riprova!");
+};
 
+
+// DADI
+
+// Generare un numero random da 1 a 6
+const palyerNumber = Math.floor(Math.random() * 6);
+document.getElementById("player-number").innerHTML = palyerNumber;
+
+const pcNumber = Math.floor(Math.random() * 6);
+document.getElementById("pc-number").innerHTML = pcNumber;
+
+if let
